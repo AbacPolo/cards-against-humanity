0989331f6e29d21323361ae55269a0bc7623d3b0
Agutr0t <abak.93@gmail.com>
2023-07-19 12:45:31 +0200

    feat: create About page

--- src/routes/about/About.js
+++ src/routes/about/About.js
@@ -4,7 +4,29 @@ import "./About.css";
 function About() {
   return (
     <div className="About_Container">
-      <div className="About_Wrapper"></div>
+      <div className="About_Wrapper">
+        <h3>Cards Against Humanity</h3>
+        <h4>Personal Project</h4>
+        <p>
+          This website is a personal project developed by me,{" "}
+          <strong>Àbac Polo</strong>, to showcase the{" "}
+          <em>Front-End Developer</em> skills learned during the{" "}
+          <em><a
+            href="https://www.codecademy.com/career-journey/front-end-engineer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Codecademy Front-End Engineer Career Path
+          </a></em>{" "}
+          studies.
+        </p>
+        <p>
+          The Cards Agains Humanity project uses the fact that <strong>Cards Against
+          Humanity®</strong> is distributed under a Creative Commons BY-NC-SA 4.0 license
+          to develop a game that allows the player to play all the combinations
+          the official cards offer.
+        </p>
+      </div>
     </div>
   );
 }
